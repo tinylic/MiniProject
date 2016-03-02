2931ea6d37c6fcdbd5fdb8cd7861f1ba518dbbb2
tinylic <lic.tiny@gmail.com>
2016-03-02 20:35:12 +0800

    final first version

--- main.cpp
+++ main.cpp
@@ -14,6 +14,66 @@ const int MAX_N = 10;
 stack<char> Stack_Operator;
 stack<bool> Stack_Number;
 vector<int> loc[MAX_N];
+vector<int> MinTerm;
+int TotalVariables = 0;
+int CountOne(int x) {
+	int ans = 0;
+	while (x) {
+		if (x & 1) ans ++;
+		x >>= 1;
+	}
+	return ans;
+}
+
+struct implication {
+	int bit;
+	int xterm;
+	int ones;
+	string exp;
+	bool used;
+	implication() {
+		bit = xterm = ones = used = 0;
+		exp = "";
+	}
+
+	implication(int mask, int _xterm) {
+		bit = mask;
+		xterm = _xterm;
+		ones = CountOne(mask);
+		exp = "";
+		for (int i = 0; i < TotalVariables; i++) {
+			if (_xterm & 1)
+				exp = exp + '-';
+			else
+				exp = exp + to_string((mask & 1) != 0);
+			_xterm = _xterm >> 1;
+			mask = mask >> 1;
+		}
+		used = 0;
+	}
+
+	string show() {
+		string ans = "";
+		for (int i = TotalVariables - 1; i >= 0; i--) {
+			bool hidden = (xterm >> i) & 1;
+			if (!hidden) {
+				ans += ('A' + TotalVariables - 1 - i);
+				if (!((bit >> i) & 1))
+					ans += '\'';
+			}
+		}
+		return ans;
+	}
+
+};
+
+vector<implication> imp, roller;
+vector<implication> primes;
+bool cmp(const implication &a, const implication &b) {
+	return a.ones < b.ones;
+}
+
+
 
 string filter(char *s) {
 	string result = "";
@@ -151,12 +211,61 @@ void solve(const string &s) {
 		cout << infix << " = " << SolvePostfix(InfixToPostfix(infix)) << endl;
 	}
 }
-int main() {
+
+void ExpressionToChart() {
 	char InString[255];
     string s;
     gets(InString);
     s = filter(InString);
     solve(s);
-	//cout << SolvePostfix(InfixToPostfix(infix)) << endl;
+}
+
+
+void ChartToExpression() {
+	imp.clear();
+	int x;
+	while ((scanf("%d", &x)) != EOF) {
+		MinTerm.push_back(x);
+	}
+	sort(MinTerm.begin(), MinTerm.end());
+    for (;(1 << TotalVariables) <= MinTerm[MinTerm.size() - 1]; TotalVariables ++);
+    cout << TotalVariables << endl;
+    for (int i = 0; i < MinTerm.size(); i++)
+		imp.push_back(implication(MinTerm[i], 0));
+	sort(imp.begin(), imp.end(), cmp);
+    while (imp.size() > 0) {
+		roller.clear();
+		for (int i = 0; i < imp.size(); i++)
+			for (int j = i + 1; j < imp.size(); j++)
+				if (imp[i].exp == imp[j].exp) {
+					imp.erase(imp.begin() + j);
+				}
+		for (int i = 0; i < imp.size(); i++)
+			for (int j = i + 1; j < imp.size(); j++)
+				if (imp[j].ones == imp[i].ones + 1 && imp[j].xterm == imp[i].xterm && CountOne(imp[j].bit ^ imp[i].bit) == 1) {
+					imp[i].used = true;
+					imp[j].used = true;
+					implication NewImp = implication(imp[i].bit, imp[i].bit ^ imp[j].bit | imp[i].xterm);
+					roller.push_back(NewImp);
+				}
+		for (int i = 0; i < imp.size(); i++)
+			if (imp[i].used == false)
+				primes.push_back(imp[i]);
+		sort(roller.begin(), roller.end(), cmp);
+		imp = roller;
+		cout << "---------------------------" << endl;
+		for (int i = 0; i < imp.size(); i++)
+			cout << imp[i].bit << " " << imp[i].xterm << " " << imp[i].exp << endl;
+    }
+    sort(primes.begin(), primes.end(), cmp);
+    for (int i = 0; i < primes.size(); i++)
+		cout << primes[i].bit << " " << primes[i].xterm << " " << primes[i].exp << endl;
+	cout << primes[0].show();
+	for (int i = 1; i < primes.size(); i++)
+		cout << " + " << primes[i].show();
+}
+int main() {
+	//ExpressionToChart();
+	ChartToExpression();
 }
 

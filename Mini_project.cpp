6e7a91f7db6c2cb691b50d2e9ba975706d8e4572
tinylic <lic.tiny@gmail.com>
2016-04-08 17:12:31 +0800

    8th Apr

--- Mini_project.cpp
+++ Mini_project.cpp
@@ -25,67 +25,67 @@ string truthtable_to_expr(const string& truth_table) {
 	return CTE.solve(truth_table);
 }
 int test_main() {
-	ExpressionToChart ETC;
-	ChartToExpression CTE;
 	//correctness testing
 	//testing expression to chart
 
 	//check sample
-	CHECK_EQUAL(ETC.solve(3, "~A | B & C"), "11010101");
+	CHECK_EQUAL(expr_to_truthtable(3, "~A | B & C"), "11010101");
 
 	//check unit input
-	CHECK_EQUAL(ETC.solve(1, "A"), "10");
-	CHECK_EQUAL(ETC.solve(2, "A"), "1010");
+	CHECK_EQUAL(expr_to_truthtable(1, "A"), "10");
+	CHECK_EQUAL(expr_to_truthtable(2, "A"), "1010");
 
 	//check complex input
-	CHECK_EQUAL(ETC.solve(4, "A & ~B ^ C & D"), "1101001000100010");
-	//CHECK_EQUAL(ETC.solve(6, "~A | B | C & D | ~E & F"), "1101001000100010");
+	CHECK_EQUAL(expr_to_truthtable(4, "A & ~B ^ C & D"), "1101001000100010");
+	CHECK_EQUAL(expr_to_truthtable(6, "~A | B | C & D | ~E & F"), "1101001000100010");
 	//testing chart to expression
 	//check all 0
-	CHECK_EQUAL(CTE.solve("0000"), "0");
+	CHECK_EQUAL(truthtable_to_expr("0000"), "0");
 	//check all 1
-	CHECK_EQUAL(CTE.solve("11"), "1");
+	CHECK_EQUAL(truthtable_to_expr("11"), "1");
 	//check sample
-	CHECK_EQUAL(CTE.solve("11010101"), "~A | B & C");
+	CHECK_EQUAL(truthtable_to_expr("11010101"), "~A | B & C");
 	//check complex input
-	CHECK_EQUAL(CTE.solve(	"1111110111011101111111111111111111111101110111011111110111011101"),
+	CHECK_EQUAL(truthtable_to_expr(	"1111110111011101111111111111111111111101110111011111110111011101"),
 							"~A | ~E & F | B | C & D");
 	//cross testing
-	CHECK_EQUAL(ETC.solve(4, CTE.solve("1101001000100010")), "1101001000100010");
-	CHECK_EQUAL(ETC.solve(6, CTE.solve(	"1111110111011101111111111111111111111101110111011111110111011101")),
-										"1111110111011101111111111111111111111101110111011111110111011101");
+	CHECK_EQUAL(expr_to_truthtable(4, truthtable_to_expr("1101001000100010")), "1101001000100010");
+	CHECK_EQUAL(expr_to_truthtable(6,
+				truthtable_to_expr(
+						"1111110111011101111111111111111111111101110111011111110111011101")),
+						"1111110111011101111111111111111111111101110111011111110111011101");
 	//robustness testing
 
 	//testing expression to chart
 
 	//check empty input
-	CHECK_THROW(ETC.solve(1, "   "), EmptyStringError);
+	CHECK_THROW(expr_to_truthtable(1, "   "), EmptyStringError);
 
 	//check invalid characters
-	CHECK_THROW(ETC.solve(2, "A + B"), InvalidCharError);
+	CHECK_THROW(expr_to_truthtable(2, "A + B"), InvalidCharError);
 
 	//check invalid variables
-	CHECK_THROW(ETC.solve(2, "A | C"), InvalidVariableError);
+	CHECK_THROW(expr_to_truthtable(2, "A | C"), InvalidVariableError);
 
 	//check syntax error
-	CHECK_THROW(ETC.solve(2, "A | ^ B"), SyntaxError);
-	CHECK_THROW(ETC.solve(1, "AA"), SyntaxError);
-	CHECK_THROW(ETC.solve(2, "A(A | B)"), SyntaxError);
+	CHECK_THROW(expr_to_truthtable(2, "A | ^ B"), SyntaxError);
+	CHECK_THROW(expr_to_truthtable(1, "AA"), SyntaxError);
+	CHECK_THROW(expr_to_truthtable(2, "A(A | B)"), SyntaxError);
 
 	//check bracket mismatching
-	CHECK_THROW(ETC.solve(2, "A ^ B)"), BracketMismatchingError);
+	CHECK_THROW(expr_to_truthtable(2, "A ^ B)"), BracketMismatchingError);
 
 	//testing chart to expression
 
 	//check empty input
-	CHECK_THROW(CTE.solve(""), EmptyStringError);
+	CHECK_THROW(truthtable_to_expr(""), EmptyStringError);
 
 	//check invalid input numbers
-	CHECK_THROW(CTE.solve("1234"), InvalidCharError);
+	CHECK_THROW(truthtable_to_expr("1234"), InvalidCharError);
 
 	//check invalid truthtable length
-	CHECK_THROW(CTE.solve("101"), InvalidLengthError);
-
+	CHECK_THROW(truthtable_to_expr("101"), InvalidLengthError);
+	return 0;
 }
 int tester() {
 	try {
